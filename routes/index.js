b355533ad05285e58576a776228ef0d07248be6d
Alex Tsiganov <alexandr.tsiganov@gmail.com>
2015-11-08 00:40:25 +0300

    adde navbar in layout.jade; implement with MySql database; setup bootstrap links

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+// router.use(function(req, res, next) {
+//   console.log('Alex: %s %s %s', req.method, req.url, req.path);
+//   next();
+// });
+//
+// // this will only be invoked if the path starts with /bar from the mount point
+// router.use('/bar', function(req, res, next) {
+//   console.log('Alex hi /bar');
+//   next();
+// });
+//
+// // always invoked
+// router.use(function(req, res, next) {
+//   res.send('Hello World');
+// });
+
 var tasks = [
-  { id: 1, title: 'Test task title with c language', lang: 'c'},
-  { id: 2, title: 'Test task title with c++ language', lang: 'c++'}];
+  { id: 1, title: 'Test task title with c language', author: 'Alex'},
+  { id: 2, title: 'Test task title with c++ language', author: 'Ivan'}];
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
@@ -11,6 +27,7 @@ router.get('/', function(req, res, next)
   res.render('index', { title: 'Golf Programming',
                         tasks: tasks});
 });
+
 
 router.get('/:id', function(req, res, next)
 {
@@ -26,6 +43,9 @@ for (var i = 0; i < tasks.length; i++)
 }
 
 });
+
+
+
 
 
 module.exports = router;
